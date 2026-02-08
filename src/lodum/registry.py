fee7fa850c5cd4ff7c0a1a7ce7d7b3b14febca52
Michael R. Bernstein <zopemaven@gmail.com>
2026-02-09 01:43:55 +0200

    legal: Update LICENSE and add SPDX headers to all Python files (Refs #63)

--- src/lodum/registry.py
+++ src/lodum/registry.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025-present Michael R. Bernstein <zopemaven@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
 from typing import Any, Callable, Dict, Optional, Type, NamedTuple, TypeVar
 from .core import Loader, Dumper
 
